d5142aad2306526c9ad546897083d3c221d1a7e4
adhirajkhubbar <141805692+adhirajkhubbar@users.noreply.github.com>
2024-03-23 10:11:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
+
+function App() {
+  const data = imageData();
+
+  return (
+    <div className="App">
+      <h1>Image Gallery</h1>
+      <div className="image-container">
+        {data.map(item => (
+          <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
